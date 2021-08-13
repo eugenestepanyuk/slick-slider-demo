3ed36fd47bfe45b199c7f909df9ef0bd07d787ee
eugenestepanyuk <erl.evgeniy@gmail.com>
2021-08-13 21:39:22 +0300

    Set default value of  initialSlide

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $('.your-class').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    initialSlide: 1,
+    // initialSlide: 1,
     // centerMode: true,
     // centerPadding: 0,
 });
