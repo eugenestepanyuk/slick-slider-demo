f0943ae0090e468cc03b67001cd964c60d0ddeea
eugenestepanyuk <erl.evgeniy@gmail.com>
2021-08-28 14:40:03 +0300

    Add custom slick slider with prev/next arrows

--- js/script.js
+++ js/script.js
@@ -1,8 +1,57 @@
-$('.your-class').slick({
+$('.slider-1').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     // initialSlide: 1,
     // centerMode: true,
     // centerPadding: 0,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+$('.slider-2').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    draggable: false,
+    // prevArrow: '<button type="button" class="slider-prev-arrow"><img src="image/slider_prev_arrow.svg" alt="prevArrow"></button>',
+    // nextArrow:
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
